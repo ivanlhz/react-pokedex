cf512a64fc585149901500d03eda747ebb27803e
ivanlhz <ivanlhz@gmail.com>
2020-02-01 07:33:22 +0000

    Rename index.js tp index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import AppRouter from './appRouter'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<AppRouter />, document.getElementById('root'))
+ReactDOM.render(<AppRouter/>, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
