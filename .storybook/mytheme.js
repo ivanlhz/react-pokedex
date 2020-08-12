af11e4b233438297c02616b9f85cc741f44e864b
Iván López Hdez <ivanlhz@gmail.com>
2020-08-12 06:16:37 +0100

    Remove Storybook

--- .storybook/mytheme.js
+++
@@ -1,9 +0,0 @@
-import { create } from '@storybook/theming/create';
-
-export default create({
-  base: 'normal',
-
-  brandTitle: 'React Pokedex',
-  brandUrl: 'https://pokereactjs.netlify.com/',
-  brandImage: 'http://pngimg.com/uploads/pokeball/pokeball_PNG19.png',
-});
